ce96b91c1a51672790d1e3b9e9a4e50c80e2dd30
Eddie Fu <99326813+eddiefu1@users.noreply.github.com>
2023-09-21 16:29:37 -0700

    Add files via upload

---
+++ GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Eddie Fu on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
